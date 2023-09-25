3cc84b8cdf581ddb3e731a7ffe40a9a9145a0134
ChaiyasitZ <s6506022610014@email.kmutnb.ac.th>
2023-09-25 23:00:36 +0700

    update

--- test.js
+++ test.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const axios = require('axios');
+var bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
-const bodyParser = require('body-parser');
+
 
 const base_url = 'http://node51818-chaiaysitz.proen.app.ruk-com.cloud';
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
