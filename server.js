09c20f889d01ab097b917a76fae6484964ab44b7
Chaiyasit Mintakorn <s6506022610014@email.kmutnb.ac.th>
2023-09-26 08:30:03 +0700

    update

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
+const path = require('path');
 
 const base_url = 'http://node51818-chaiaysitz.proen.app.ruk-com.cloud';
 
+app.set('views', path.join(__dirname, 'public/views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', async (req, res) => {
